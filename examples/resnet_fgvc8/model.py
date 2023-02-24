c3ce9ba575ef9d3732ebd85c5cecf0721346f22b
xsdeep <408419849@qq.com>
2023-02-24 21:26:20 +0800

    update

--- examples/resnet_fgvc8/model.py
+++ examples/resnet_fgvc8/model.py
@@ -10,7 +10,7 @@ from composer.models import ComposerClassifier
 from torchmetrics import Accuracy, MetricCollection
 
 from sunyata.pytorch.arch.base import Residual
-from sunyata.pytorch_lightning.base import BaseModule
+# from sunyata.pytorch_lightning.base import BaseModule
 from sunyata.pytorch.arch.foldnet import FoldNet, FoldNetCfg, FoldNetRepeat, FoldNetRepeat2, Block2
 #from sunyata.pytorch.arch.bayes.core import log_bayesian_iteration
 
