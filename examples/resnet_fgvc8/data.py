5a59b547e0d9b6bd618e63eb1f417212de1da46d
xsdeep <408419849@qq.com>
2023-03-01 22:14:53 +0800

    add colorjitter

--- examples/resnet_fgvc8/data.py
+++ examples/resnet_fgvc8/data.py
@@ -113,8 +113,8 @@ def build_imagenet_dataspec(
             transforms.RandomVerticalFlip(),
             transforms.RandAugment(num_ops=2, magnitude=12),
             transforms.ColorJitter(0.3, 0.3, 0.3),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            #transforms.ToTensor(),
+            #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 
         ]
     # else:
