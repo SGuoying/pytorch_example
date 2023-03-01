bf6d28a6babac6dfa004785803708542d15a720a
xsdeep <408419849@qq.com>
2023-03-01 15:28:01 +0800

    block=config.model.Block2

--- examples/resnet_fgvc8/main.py
+++ examples/resnet_fgvc8/main.py
@@ -67,7 +67,7 @@ def main(config):
         shuffle=True,
         resize_size=config.train_dataset.resize_size,
         crop_size=config.train_dataset.crop_size,
-        num_workers=2,  #此处待修改config.train_dataset.num_workers
+        num_workers=config.train_dataset.num_workers,
         pin_memory=True,
         persistent_workers=True)
 
@@ -85,7 +85,7 @@ def main(config):
         shuffle=False,
         resize_size=config.eval_dataset.resize_size,
         crop_size=config.eval_dataset.crop_size,
-        num_workers=2,   #此处待修改config.eval_dataset.num_workers
+        num_workers=config.eval_dataset.num_workers,
         pin_memory=True,
         persistent_workers=True)
     print('Built evaluation dataloader\n')
@@ -95,6 +95,7 @@ def main(config):
     composer_model = build_composer_resnet( 
         model_name=config.model.name,
         loss_name=config.model.loss_name,
+        block=config.model.Block2,
         hidden_dim=config.model.hidden_dim,
         kernel_size=config.model.kernel_size,
         patch_size=config.model.patch_size,
