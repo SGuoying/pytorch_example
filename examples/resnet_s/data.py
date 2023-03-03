294635ea8c693121fbfd3cffc45f386972162b18
SGuoying <826367045@qq.com>
2023-03-03 09:49:48 +0800

    update

--- examples/resnet_s/data.py
+++ examples/resnet_s/data.py
@@ -10,10 +10,10 @@ import torch
 from composer.core import DataSpec
 from composer.datasets.utils import NormalizationFn, pil_image_collate
 from composer.utils import dist
-from streaming import StreamingDataset
+
 from torch.utils.data import DataLoader
 from torchvision import transforms
-from torchvision.datasets import ImageFolder, VisionDataset
+from sunyata.pytorch.data.tiny_imagenet import TinyImageNet
 
 # Scale by 255 since the collate `pil_image_collate` results in images in range 0-255
 # If using ToTensor() and the default collate, remove the scaling by 255
@@ -21,49 +21,49 @@ IMAGENET_CHANNEL_MEAN = (0.485 * 255, 0.456 * 255, 0.406 * 255)
 IMAGENET_CHANNEL_STD = (0.229 * 255, 0.224 * 255, 0.225 * 255)
 
 
-class StreamingImageNet(StreamingDataset, VisionDataset):
-    """ImageNet streaming dataset based on PyTorch's VisionDataset.
+# class StreamingImageNet(StreamingDataset, VisionDataset):
+#     """ImageNet streaming dataset based on PyTorch's VisionDataset.
 
-    Args:
-        remote (str): Remote directory (S3 or local filesystem) where dataset is stored.
-        local (str): Local filesystem directory where dataset is cached during operation.
-        split (str): The dataset split to use, either 'train' or 'val'.
-        shuffle (bool): Whether to iterate over the samples in randomized order.
-        transform (callable, optional): A function/transform that takes in an image and returns a transformed version.
-            Default: ``None``.
-        batch_size (int, optional): The batch size that will be used on each device's DataLoader.
-            Default: ``None``.
-    """
+#     Args:
+#         remote (str): Remote directory (S3 or local filesystem) where dataset is stored.
+#         local (str): Local filesystem directory where dataset is cached during operation.
+#         split (str): The dataset split to use, either 'train' or 'val'.
+#         shuffle (bool): Whether to iterate over the samples in randomized order.
+#         transform (callable, optional): A function/transform that takes in an image and returns a transformed version.
+#             Default: ``None``.
+#         batch_size (int, optional): The batch size that will be used on each device's DataLoader.
+#             Default: ``None``.
+#     """
 
-    def __init__(self,
-                 remote: Optional[str],
-                 local: str,
-                 split: Optional[str],
-                 shuffle: bool,
-                 transform: Optional[Callable] = None,
-                 batch_size: Optional[int] = None) -> None:
+#     def __init__(self,
+#                  remote: Optional[str],
+#                  local: str,
+#                  split: Optional[str],
+#                  shuffle: bool,
+#                  transform: Optional[Callable] = None,
+#                  batch_size: Optional[int] = None) -> None:
 
-        if split not in ['train', 'val']:
-            raise ValueError(
-                f"split='{split}', but must be one of ['train', 'val'].")
+#         if split not in ['train', 'val']:
+#             raise ValueError(
+#                 f"split='{split}', but must be one of ['train', 'val'].")
 
-        self.transform = transform
+#         self.transform = transform
 
-        super().__init__(remote=remote,
-                         local=local,
-                         split=split,
-                         shuffle=shuffle,
-                         batch_size=batch_size)
+#         super().__init__(remote=remote,
+#                          local=local,
+#                          split=split,
+#                          shuffle=shuffle,
+#                          batch_size=batch_size)
 
-    def __getitem__(self, idx: int) -> Any:
-        sample = super().__getitem__(idx)
-        image = sample['x']
-        if image.mode != 'RGB':
-            image = image.convert('RGB')
-        if self.transform:
-            image = self.transform(image)
-        target = sample['y']
-        return image, target
+#     def __getitem__(self, idx: int) -> Any:
+#         sample = super().__getitem__(idx)
+#         image = sample['x']
+#         if image.mode != 'RGB':
+#             image = image.convert('RGB')
+#         if self.transform:
+#             image = self.transform(image)
+#         target = sample['y']
+#         return image, target
 
 
 def build_imagenet_dataspec(
@@ -94,43 +94,47 @@ def build_imagenet_dataspec(
         crop_size (int, optional): The crop size to use. Default: ``224``.
         **dataloader_kwargs (Dict[str, Any]): Additional settings for the dataloader (e.g. num_workers, etc.)
     """
-    if is_streaming and not local:
-        raise ValueError(
-            '`local` argument must be specified if using a streaming dataset.')
+    # if is_streaming and not local:
+    #     raise ValueError(
+    #         '`local` argument must be specified if using a streaming dataset.')
 
     split = 'train' if is_train else 'val'
     transform: List[torch.nn.Module] = []
-    if resize_size > 0:
-        transform.append(transforms.Resize(resize_size))
+    # if resize_size > 0:
+    #     transform.append(transforms.Resize(resize_size))
 
     # Add split specific transformations
     if is_train:
         transform += [
-            transforms.RandomResizedCrop(crop_size,
-                                         scale=(0.08, 1.0),
-                                         ratio=(0.75, 4.0 / 3.0)),
+            # transforms.RandomResizedCrop(crop_size,
+            #                              scale=(0.08, 1.0),
+            #                              ratio=(0.75, 4.0 / 3.0)),
             transforms.RandomHorizontalFlip()
         ]
-    else:
-        transform.append(transforms.CenterCrop(crop_size))
+    # else:
+    #     transform.append(transforms.CenterCrop(crop_size))
 
     transform = transforms.Compose(transform)
 
     device_transform_fn = NormalizationFn(mean=IMAGENET_CHANNEL_MEAN,
                                           std=IMAGENET_CHANNEL_STD)
-    if is_streaming:
-        assert local is not None  # for type checking
-        dataset = StreamingImageNet(remote=data_path,
-                                    local=local,
-                                    split=split,
-                                    shuffle=shuffle,
-                                    transform=transform)  # type: ignore
-        sampler = None
-    else:
-        dataset = ImageFolder(os.path.join(data_path, split), transform)
-        sampler = dist.get_sampler(dataset,
-                                   drop_last=drop_last,
-                                   shuffle=shuffle)
+    dataset = TinyImageNet(root=data_path, split=split, transform=transform, 
+                           download=True)
+    sampler = dist.get_sampler(dataset, drop_last=drop_last,
+                               shuffle=shuffle)
+    # if is_streaming:
+    #     assert local is not None  # for type checking
+    #     dataset = StreamingImageNet(remote=data_path,
+    #                                 local=local,
+    #                                 split=split,
+    #                                 shuffle=shuffle,
+    #                                 transform=transform)  # type: ignore
+    #     sampler = None
+    # else:
+    #     dataset = ImageFolder(os.path.join(data_path, split), transform)
+    #     sampler = dist.get_sampler(dataset,
+    #                                drop_last=drop_last,
+    #                                shuffle=shuffle)
 
     # DataSpec allows for on-gpu transformations, slightly relieving dataloader bottleneck
     return DataSpec(
