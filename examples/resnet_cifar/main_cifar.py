3587068291de01e5b6d0e348d5da2ee98cb7aab2
SGuoying <826367045@qq.com>
2023-03-31 11:10:09 +0800

    update

--- examples/resnet_cifar/main_cifar.py
+++ examples/resnet_cifar/main_cifar.py
@@ -194,7 +194,7 @@ def main(config):
         # save_num_checkpoints_to_keep=config.save_num_checkpoints_to_keep,
         save_overwrite=config.get('save_overwrite', False),
         load_path=config.load_path,
-        load_ignore_keys=["state_dict/model/fc.weight", "state_dict/model/fc.bias",],
+        load_ignore_keys=["state/model/fc.weight*", "state/model/fc.bias*",],
         load_exclude_algorithms = ["BlurPool"],
         device=device,
         precision=precision,
